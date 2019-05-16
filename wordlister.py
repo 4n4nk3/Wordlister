bdf3105607d35a6d7c4148891d0109dcaf23e013
Francesco <47717886+4n4nk3@users.noreply.github.com>
2019-05-16 12:32:13 +0200

    Update wordlister.py

--- wordlister.py
+++ wordlister.py
@@ -1,10 +1,10 @@
 import itertools
-import sys
-import multiprocessing
+from sys import argv, exit
+from multiprocessing import Pool
 
-if len(sys.argv) < 4:
-    print('Missing arguments:\n\t{} [input_file] [min-length] [max-length]'.format(sys.argv[0]))
-    sys.exit()
+if len(argv) < 4:
+    print('Missing arguments:\n\t{} [input_file] [min-length] [max-length] [n-combinations]'.format(argv[0]))
+    exit()
 
 
 def printer(combo):
@@ -12,7 +12,7 @@ def printer(combo):
     length = len(line)
     if length == 0:
         return False
-    elif int(sys.argv[2]) <= length <= int(sys.argv[3]):
+    elif int(argv[2]) <= length <= int(argv[3]):
         for word_check in check_list:
             if line.lower().count(word_check) > 1:
                 return True
@@ -24,39 +24,17 @@ def printer(combo):
     return True
 
 
-leet_replacements = (
-    ('o', '0'), ('O', '0'), ('a', '4'), ('A', '4'), ('e', '3'), ('E', '3'), ('i', '1'), ('I', '1'), ('s', '5'),
-    ('S', '5'))
-
-input_list = set()
-check_list = set()
-
-with open(sys.argv[1], 'r') as input_file:
-    for row in input_file:
-        word = row.rstrip('\n')
-        check_list.add(word)
-        input_list.add(word)
-        input_list.add(word.capitalize())
-        input_list.add(word.swapcase())
-
-iterators = set()
-n_perm = 5
-for x in range(n_perm):
-    x += 1
-    iterators.add(itertools.permutations(input_list, x))
-
-slices = set()
-step = 1000000
-for iterator in iterators:
+def slice_and_run(single_iterator):
+    step = 10000000
     start = 0
     stop = start + step
     next_it = False
     while 1:
         if next_it is False:
-            cake_slice = itertools.islice(iterator, start, stop)
+            cake_slice = itertools.islice(single_iterator, start, stop)
         else:
-            cake_slice = itertools.islice(iterator, start, None)
-        with multiprocessing.Pool() as pool:
+            cake_slice = itertools.islice(single_iterator, start, None)
+        with Pool() as pool:
             data = pool.map(printer, cake_slice)
         start += step
         stop += step
@@ -64,3 +42,22 @@ for iterator in iterators:
             break
         if len(data) == 0:
             next_it = True
+
+
+leet_replacements = (
+    ('o', '0'), ('O', '0'), ('a', '4'), ('A', '4'), ('e', '3'), ('E', '3'), ('i', '1'), ('I', '1'), ('s', '5'),
+    ('S', '5'))
+
+input_list = set()
+check_list = set()
+
+with open(argv[1], 'r') as input_file:
+    for row in input_file:
+        word = row.rstrip('\n')
+        check_list.add(word)
+        input_list.add(word)
+        input_list.add(word.capitalize())
+        input_list.add(word.swapcase())
+
+for x in range(int(argv[4])):
+    slice_and_run(itertools.permutations(input_list, x+1))
