c95f4afe23d1fdcea5a734a872c31b859255c5b8
Francesco <47717886+4n4nk3@users.noreply.github.com>
2019-05-16 11:56:14 +0200

    Update wordlister.py slicing
    First basically introduction of slicing in order to prevent crash from excessive RAM memory usage.

--- wordlister.py
+++ wordlister.py
@@ -2,53 +2,65 @@ import itertools
 import sys
 import multiprocessing
 
-global check_list
-global min_length
-global max_length
-global leet_replacements
-
 if len(sys.argv) < 4:
-    print('Missing arguments:\n\t{} [input_file] [min-lenght] [max-lenght]'.format(sys.argv[0]))
+    print('Missing arguments:\n\t{} [input_file] [min-length] [max-length]'.format(sys.argv[0]))
     sys.exit()
-
-min_length = int(sys.argv[2])
-max_length = int(sys.argv[3])
 
 
 def printer(combo):
-    line: str = ''.join(combo)
+    line = ''.join(combo)
     length = len(line)
-    if length >= min_length and length <= max_length:
-        for word in check_list:
-            if line.lower().count(word) > 1:
-                return False
+    if length == 0:
+        return False
+    elif int(sys.argv[2]) <= length <= int(sys.argv[3]):
+        for word_check in check_list:
+            if line.lower().count(word_check) > 1:
+                return True
         print(line)
         # Leet
         for old, new in leet_replacements:
             line = line.replace(old, new)
         print(line)
-        return True
+    return True
 
 
 leet_replacements = (
-    ('o', '0'), ('O', '0'), ('a', '4'), ('A', '4'), ('e', '3'), ('E', '3'), ('g', '9'), ('G', '9'), ('i', '1'),
-    ('I', '1'), ('l', '1'), ('L', '1'), ('s', '5'), ('S', '5'), ('t', '7'), ('T', '7'))
+    ('o', '0'), ('O', '0'), ('a', '4'), ('A', '4'), ('e', '3'), ('E', '3'), ('i', '1'), ('I', '1'), ('s', '5'),
+    ('S', '5'))
 
 input_list = set()
 check_list = set()
 
 with open(sys.argv[1], 'r') as input_file:
     for row in input_file:
-        word = row[:-1]
-        # Plain text
+        word = row.rstrip('\n')
         check_list.add(word)
         input_list.add(word)
         input_list.add(word.capitalize())
         input_list.add(word.swapcase())
 
-jobs = []
+iterators = set()
 n_perm = 5
 for x in range(n_perm):
     x += 1
-    with multiprocessing.Pool() as pool:
-        pool.map(printer, itertools.permutations(input_list, x))
+    iterators.add(itertools.permutations(input_list, x))
+
+slices = set()
+step = 1000000
+for iterator in iterators:
+    start = 0
+    stop = start + step
+    next_it = False
+    while 1:
+        if next_it is False:
+            cake_slice = itertools.islice(iterator, start, stop)
+        else:
+            cake_slice = itertools.islice(iterator, start, None)
+        with multiprocessing.Pool() as pool:
+            data = pool.map(printer, cake_slice)
+        start += step
+        stop += step
+        if next_it is True:
+            break
+        if len(data) == 0:
+            next_it = True
