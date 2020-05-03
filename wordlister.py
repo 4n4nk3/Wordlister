a98ee0c49163ec7ed895e6710df8fb1f0e873d82
4n4nk3 <47717886+4n4nk3@users.noreply.github.com>
2020-05-03 14:49:35 +0200

    Changed print() calls to use f'{}' strings. Cleaned up slice_and_run() function. Improved type inting. Improved read_input_file() function.

--- wordlister.py
+++ wordlister.py
@@ -1,15 +1,15 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-"""Wordlister, a simple wordlist generator and mangler written in python.\n"""
+#!/usr/bin/python3
+"""Wordlister, a simple wordlist generator and mangler written in python 3.8."""
 
-# Written By Ananke: https://github.com/4n4nk3
-from itertools import permutations, islice
-from multiprocessing import Pool
 import argparse
+# Written By Ananke: https://github.com/4n4nk3
+from itertools import islice, permutations
+from multiprocessing import Pool
+from sys import exit
 
 
 def init_argparse() -> argparse.ArgumentParser:
-    """Define and manage arguments passed to Wordlister via terminal.\n"""
+    """Define and manage arguments passed to Wordlister via terminal."""
     parser = argparse.ArgumentParser(
         description='A simple wordlist generator and mangler written in python.')
     required = parser.add_argument_group('required arguments')
@@ -37,48 +37,34 @@ def init_argparse() -> argparse.ArgumentParser:
     return parser
 
 
-def printer(combo_printer: list):
-    """Print generated words to stdout and in case apply chosen mutagens (append, prepend, leet).\n"""
+def printer(combo_printer: list) -> bool:
+    """Print generated words to stdout and in case apply chosen mutagens (append, prepend, leet)."""
     if len(set(map(str.lower, combo_printer))) == len(combo_printer):
         line_printer = ''.join(combo_printer)
         if args.min <= len(line_printer) <= args.max:
             print(line_printer)
-            if args.append is not None:
-                print(line_printer + args.append)
-            if args.prepend is not None:
-                print(args.prepend + line_printer)
+            if args.append is not None and len(line_printer) + len(args.append) <= args.max:
+                print(f'{line_printer}{args.append}')
+            if args.prepend is not None and len(line_printer) + len(args.prepend) <= args.max:
+                print(f'{args.prepend}{line_printer}')
             if args.leet is True:
                 leet(line_printer)
     return True
 
 
-def slice_and_run(single_iterator: permutations):
-    """Makes slices from iterator and process them via a process pool.\n"""
+def slice_and_run(single_iterator: iter):
+    """Makes slices from iterator and process them via a process pool."""
     step = 10000000
     start = 0
-    stop = start + step
-    # I use next_it bool to make sure to create one more slice with no end limit when slices are finished
-    next_it = False
-    while True:
-        if next_it is False:
-            cake_slice = islice(single_iterator, start, stop)
-        else:
-            cake_slice = islice(single_iterator, start, None)
-        if args.cores is None:
-            with Pool() as pool:
-                data = pool.map(printer, cake_slice)
-        else:
-            with Pool(args.cores) as pool:
-                data = pool.map(printer, cake_slice)
-        start += step
-        stop += step
-        if next_it is True:
-            break
-        if not data:
-            next_it = True
+    data = True
+    with Pool(args.cores) as pool:
+        while data:
+            cake_slice = islice(single_iterator, start, start + step)
+            data = pool.map(printer, cake_slice)
+            start += step
 
 
-def my_replace(line_to_mutate):
+def my_replace(line_to_mutate: str) -> str:
     leet_replacements = (('o', '0'), ('O', '0'), ('a', '4'), ('A', '4'), ('e', '3'), ('E', '3'),
                          ('i', '1'), ('I', '1'), ('s', '5'), ('S', '5'))
     for old_printer, new_printer in leet_replacements:
@@ -87,17 +73,17 @@ def my_replace(line_to_mutate):
 
 
 def leet(line_leet: str):
-    """Apply leet mutagen and then if needed apply append and prepend to leeted version of the string.\n"""
+    """Apply leet mutagen and then if needed apply append and prepend to leeted version of the string."""
     line_leet = my_replace(line_leet)
     print(line_leet)
     if args.append is not None:
-        print(line_leet + args.append)
+        print(f'{line_leet}{args.append}')
     if args.prepend is not None:
-        print(args.prepend + line_leet)
+        print(f'{args.prepend}{line_leet}')
 
 
-def test_printer(x_test: int, out_counter_test: int):
-    """Test printer.\n"""
+def test_printer(x_test: int, out_counter_test: int) -> int:
+    """Test printer."""
     input_list = read_input_file(args.input)
     for combo in permutations(input_list, x_test + 1):
         if len(set(map(str.lower, combo))) == len(combo):
@@ -108,12 +94,12 @@ def test_printer(x_test: int, out_counter_test: int):
                 if out_counter_test >= args.test:
                     return out_counter_test
                 if args.append is not None:
-                    print(line + args.append)
+                    print(f'{line}{args.append}')
                     out_counter_test += 1
                     if out_counter_test >= args.test:
                         return out_counter_test
                 if args.prepend is not None:
-                    print(args.prepend + line)
+                    print(f'{args.prepend}{line}')
                     out_counter_test += 1
                     if out_counter_test >= args.test:
                         return out_counter_test
@@ -124,38 +110,38 @@ def test_printer(x_test: int, out_counter_test: int):
                     if out_counter_test >= args.test:
                         return out_counter_test
                     if args.append is not None:
-                        print(line + args.append)
+                        print(f'{line}{args.append}')
                         out_counter_test += 1
                         if out_counter_test >= args.test:
                             return out_counter_test
                     if args.prepend is not None:
-                        print(args.prepend + line)
+                        print(f'{args.prepend}{line}')
                         out_counter_test += 1
                         if out_counter_test >= args.test:
                             return out_counter_test
     return out_counter_test
 
 
-def read_input_file(file_path):
-    """Read input file and return a set of words.\n"""
-    input_list = set()
+def read_input_file(file_path: str) -> set:
+    """Read input file and return a set of words."""
     try:
         with open(file_path, 'r') as input_file:
-            for row in input_file:
-                word = row.rstrip('\n')
-                input_list.add(word)
-                # Apply capitalize mutagen and upper mutagen if needed
-                if args.cap is True:
-                    input_list.add(word.capitalize())
-                if args.up is True:
-                    input_list.add(word.upper())
+            data = input_file.read().splitlines()
     except FileNotFoundError:
         print('Input file not found!\nExiting...')
+        exit(1)
+    # Apply capitalize mutagen and upper mutagen if needed
+    input_list = set(data)
+    for word in data:
+        if args.cap is True:
+            input_list.add(word.capitalize())
+        if args.up is True:
+            input_list.add(word.upper())
     return input_list
 
 
 def test_run():
-    """Test run to generate only N words.\n"""
+    """Test run to generate only N words."""
     out_counter = 0
     n_perm = args.perm
     n_test = args.test
@@ -166,7 +152,7 @@ def test_run():
 
 
 def real_run():
-    """Real run.\n"""
+    """Real run."""
     input_list = read_input_file(args.input)
     n_perm = args.perm
     for x in range(n_perm):
